0a9ab1b8abbdcba5154bfbb20db2791dcc8a0d31
balbu <lancien.louis@gmail.com>
2022-12-12 22:30:33 +0100

    dev: ajout d'une connection à une base de donnée

--- src/recette/entities/recette.entity.ts
+++ src/recette/entities/recette.entity.ts
@@ -1,1 +1,37 @@
-export class Recette {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+import { HydratedDocument } from 'mongoose';
+
+export type RecetteDocument = HydratedDocument<Recette>;
+export enum EMidiSoir {
+  midi = 'midi',
+  soir = 'soir',
+}
+export enum EDessertEntreePlat {
+  dessert = 'dessert',
+  entree = 'entrée',
+  plat = 'plat',
+}
+
+@Schema()
+export class Recette {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  imageUri: number;
+
+  @Prop({ required: false })
+  recetteUri: string;
+
+  @Prop({ required: true })
+  tagMidiSoir: EMidiSoir[];
+
+  @Prop({ required: true })
+  tagDessertEntreePlat: EDessertEntreePlat[];
+
+  @Prop({ required: false })
+  jourProg: Date[];
+}
+
+export const RecetteSchema = SchemaFactory.createForClass(Recette);
